e0c82667f08e78a5d569cbd0e092542ead27dadd
Chris Olah <31199188+Colah2122@users.noreply.github.com>
2022-02-02 11:12:16 -0500

    Create main.cpp

---
+++ Project1A/main.cpp
@@ -0,0 +1,16 @@
+/************************************************************
+Author: Chris Olah
+Course: Computing IV (COMP.2040)
+Assignment: Project 1A
+Professor: Dr. Daly
+Date: February 1 2022
+************************************************************/
+
+#include <iostream> 
+#include "FibLFSR.h"
+
+// Including main.cpp source file for Makefile
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
