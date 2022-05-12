586c0ce46b334ea506da3fbc5f615a165f975c4b
Chris Olah <31199188+Colah2122@users.noreply.github.com>
2022-05-12 13:05:16 -0400

    Add files via upload

---
+++ Solar System Simulation/CelestialBody.cpp
@@ -0,0 +1,115 @@
+#include "CelestialBody.h"
+
+CelestialBody::CelestialBody()
+{
+  return;
+}
+
+CelestialBody::CelestialBody(double xposition, double yposition, double xvelocity, double yvelocity, double mass,string filename)
+{
+  _xposition = xposition;
+  _yposition = yposition;
+  _xvelocity = xvelocity;
+  _yvelocity = yvelocity;
+  _mass = mass;
+  _filename = filename;
+  texture.loadFromImage(image);
+  sprite.setTexture(texture);
+  sprite.setPosition(sf::Vector2f(_xposition, _yposition));
+}
+
+istream& operator>> (istream &input, CelestialBody &b)
+{
+  input >> b._xposition >> b._yposition;
+  input >> b._yvelocity >> b._yvelocity;
+  input >> b._mass >> b._filename;
+
+  b.texture.loadFromFile(b._filename);
+  b.sprite.setTexture(b.texture);
+  b.sprite.setPosition(sf::Vector2f(b._xposition, b._yposition)); //intial
+
+  b.acceleration_x = 0;
+  b.acceleration_y = 0;
+
+  b.force_x = 0;
+  b.force_y = 0;
+
+  return input;
+}
+
+void CelestialBody::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+  target.draw(sprite);
+}
+
+
+void CelestialBody::radius(float radius)
+{
+    _radius = radius;
+}
+
+void CelestialBody::forces(double xForce, double yForce)
+{
+  force_x = xForce;
+  force_y = yForce;
+}
+
+
+void CelestialBody::position()//done
+{ 
+  double newxposition = ((_xposition / _radius) * (width / 2)) + (height / 2);                      
+  double newyposition = ((_yposition / _radius) * (width / 2)) + (height / 2);                     
+
+  sprite.setPosition(sf::Vector2f(newyposition, newxposition));
+}
+
+double Universe::calc_force_x(CelestialBody& first, CelestialBody& second)
+{
+ const double G = 6.67e-11;
+ double delta_x = second.get_x() - first.get_x();
+ double delta_y = second.get_y() - first.get_y();
+ double r = pow(delta_x,2) + pow (delta_y, 2);
+ double sqrtr = sqrt(r);
+
+ double f = ( G * first.get_mass() * second.get_mass()) / r;
+ double F = f* (delta_x /sqrtr);
+
+ return F;
+
+}
+
+double Universe::calc_force_y(CelestialBody& first, CelestialBody& second)
+{
+ const double G = 6.67e-11;
+
+ double delta_x = second.get_x() - first.get_x();
+ double delta_y = second.get_y() - first.get_y();
+ double r = pow(delta_x,2) + pow (delta_y, 2);
+ double sqrtr = sqrt(r);
+
+ double f = ( G * first.get_mass() * second.get_mass()) / r;
+ double F = f* (delta_y /sqrtr);
+
+ return F;
+ 
+}
+
+void CelestialBody::step(double t)
+{
+
+  acceleration_x = force_x / _mass;
+  acceleration_y = force_y / _mass;
+
+  _xvelocity = _xvelocity + (acceleration_x * t);
+  _yvelocity = _yvelocity + (acceleration_y * t);
+
+  _xposition = _xposition + ( _xvelocity * t);
+  _yposition = _yposition + ( _yvelocity * t);
+
+}
+
+
+
+
+
+
---
+++ Solar System Simulation/CelestialBody.h
@@ -0,0 +1,95 @@
+/************************************************************
+* @file CelestialBody.h
+* @author: Chris Olah
+* @copyright Umass Lowell February 22 2022
+* @version 1.0
+* Course: Computing IV (COMP.2040)
+* Assignment: Project 2B
+* Professor: Dr. Daly
+************************************************************/
+
+#ifndef CelestialBody_H
+#define CelestialBody_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <memory>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+using namespace std;
+const int height = 512;
+const int width = 512;
+
+class CelestialBody:public sf::Drawable
+{
+    public:
+
+    CelestialBody();//default
+    CelestialBody(double xposition, double yposition, double xvelocity, double yvelocity, double mass, string filename); // Constructor
+    friend istream& operator>> (istream &input, CelestialBody &b);//overloaded
+    void radius (float radius);
+    void position();
+
+    void forces (double xForce, double yForce);//used
+    void step (double time);
+
+    double get_mass()
+    {
+        return _mass;
+    }
+
+    double get_x()
+    {
+        return _xposition;
+    }
+
+    double get_y()
+    {
+        return _yposition;
+    }
+
+    private:
+
+    void virtual draw(sf::RenderTarget& target, sf::RenderStates states) const;
+    
+    double _xposition, _yposition;
+    double _xvelocity, _yvelocity;
+    double _mass;
+    double _radius;
+    string _filename;
+
+
+    double acceleration_x;
+    double acceleration_y;
+
+    double force_x;
+    double force_y;
+
+    sf::Image image;
+    sf::Sprite sprite;
+    sf::Texture texture;
+    
+};
+
+class Universe
+{
+    public:
+    vector<shared_ptr<CelestialBody> >body;
+    
+    double calc_force_x (CelestialBody &first, CelestialBody &second);
+    double calc_force_y (CelestialBody &first, CelestialBody &second);
+    
+    private:
+    double seconds;
+    double net_force;
+
+};
+
+#endif
